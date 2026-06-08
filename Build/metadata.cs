67b4dfa12c3b5553eb76a89d6b8742255630512d
NotDifficult <89353219+notdifficult@users.noreply.github.com>
2026-06-09 06:15:20 +0700

    build
    build

---
+++ Build/metadata.cs
@@ -0,0 +1,20 @@
+using SPTarkov.Server.Core.Models.Spt.Mod;
+using Range = SemanticVersioning.Range;
+using Version = SemanticVersioning.Version;
+
+namespace props;
+
+public record metadata : AbstractModMetadata
+{
+    public override string ModGuid { get; init; } = "com.NoTDifficult.SPT-NoTDifficult-Props";
+    public override string Name { get; init; } = "SPT-NoTDifficult-Props";
+    public override string Author { get; init; } = "NoTDifficult";
+    public override List<string>? Contributors { get; init; } = null;
+    public override Version Version { get; init; } = new Version("1.0.0");
+    public override Range SptVersion { get; init; } = new Range("~4.0.13");
+    public override List<string>? Incompatibilities { get; init; } = null;
+    public override Dictionary<string, Range>? ModDependencies { get; init; } = null;
+    public override string? Url { get; init; } = "https://github.com/notdifficult/SPT-NoTDifficult-Props";
+    public override bool? IsBundleMod { get; init; } = true;
+    public override string License { get; init; } = "MIT";
+}
---
+++ Build/plugin.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.DI;
+using SPTarkov.Server.Core.Models.Logging;
+using SPTarkov.Server.Core.Models.Utils;
+using WTTServerCommonLib.Services;
+
+namespace SPTNoTDifficultProps;
+
+[Injectable(InjectionType = InjectionType.Singleton, TypePriority = OnLoadOrder.PostDBModLoader + 2)]
+
+public class Plugin(ISptLogger<Plugin> props, WTTCustomItemServiceExtended itemService) : IOnLoad
+{
+    private static Assembly Assembly = Assembly.GetExecutingAssembly();
+    
+    public Task OnLoad()
+    {
+        props.LogWithColor("Hello NoTDifficultProps Loading...",LogTextColor.Cyan);
+
+        itemService.CreateCustomItems(Assembly);
+        
+        return Task.CompletedTask; 
+    }
+}
